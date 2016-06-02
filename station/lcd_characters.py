c00debb70423a7f0f984e32c39de07a224914e0c
Corey Beres <corey.beres@gmail.com>
2016-06-02 08:18:48 -0400

    rounder degree symbol

--- station/lcd_characters.py
+++ station/lcd_characters.py
@@ -4,10 +4,10 @@ class LcdCharacter:
         self.bitmap = bitmap
 
 degree = LcdCharacter(0, (
-    0b00000,
-    0b01110,
-    0b01010,
-    0b01110,
+    0b00110,
+    0b01001,
+    0b01001,
+    0b00110,
     0b00000,
     0b00000,
     0b00000,
