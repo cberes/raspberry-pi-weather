97a177ca259e98b3824a906fa9913bc680899c57
Corey Beres <corey.beres@gmail.com>
2016-06-05 06:24:45 -0400

    fix division by zero

--- station/circuits/voltage_divider.py
+++ station/circuits/voltage_divider.py
@@ -5,4 +5,4 @@ class VoltageDivider(object):
         self.r_load2 = r_load2
 
     def get_resistance(self, v_out):
-        return self.v_dc / v_out * self.r_load2 - self.r_load1 - self.r_load2
+        return 0 if v_out == 0 else (self.v_dc / v_out * self.r_load2 - self.r_load1 - self.r_load2)
--- station/circuits/voltage_load.py
+++ station/circuits/voltage_load.py
@@ -4,4 +4,4 @@ class VoltageLoad(object):
         self.r_load = r_load
 
     def get_resistance(self, v_out):
-        return self.v_dc / v_out * self.r_load - self.r_load
+        return 0 if v_out == 0 else (self.v_dc / v_out * self.r_load - self.r_load)
